e53ad856ae50e411788a352ed3c96060c8ce5654
Mitchell Marsh <mitchell@marshhome.net>
2018-01-09 18:57:53 -0800

    Add project files.

---
+++ Marine_Permit_Palace/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Marine_Permit_Palace.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

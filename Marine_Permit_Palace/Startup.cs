fa4fc4f6b926846aa41bd59ce2bea71de6b48d67
Mitchell Marsh <mmarsh@scale7.co>
2018-03-19 23:40:24 -0700

    Some changes

--- Marine_Permit_Palace/Startup.cs
+++ Marine_Permit_Palace/Startup.cs
@@ -38,7 +38,7 @@ namespace Marine_Permit_Palace
 
             //Scoped
             services.AddScoped<IDocumentService, DocumentService>();
-            services.AddScoped<ISubmittedDocumentService, SubmittedDocument>();
+            services.AddScoped<ISubmittedDocumentService, SubmittedDocumentService>();
             services.AddScoped<IUserDocumentIntermediateService, UserDocumentIntermediateService>();
 
 

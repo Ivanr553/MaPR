3432dc241c7ab5aa2498afccb36e9dc34338b812
Mitchell Marsh <mmarsh@scale7.co>
2018-04-09 20:24:34 -0700

    Docuemnt Servicing

--- Marine_Permit_Palace/Controllers/DocumentSaveController.cs
+++ Marine_Permit_Palace/Controllers/DocumentSaveController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,6 +20,10 @@ namespace Marine_Permit_Palace.Controllers
             _SubmittedDocumentService = isds;
         }
 
+        public IActionResult GetAllDocuments()
+        {
+            return Json(_DocumentSerivce.GetAll<Document>());
+        }
 
         public IActionResult GetNewAutopopulatedFile(string document_id)
         {
@@ -27,6 +32,8 @@ namespace Marine_Permit_Palace.Controllers
             {
                 //Grab the desired file
                 Document document = _DocumentSerivce.Get(id);
+                MemoryStream PDF_Mem = new MemoryStream(System.IO.File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "dist", "documents", document.TemplateName)));
+                return new FileStreamResult(PDF_Mem, "application/pdf");
                 //populate all the known fields
                 //return to the populated file
             }
@@ -34,12 +41,6 @@ namespace Marine_Permit_Palace.Controllers
             {
                 return Json(new Result("Failure", "Incorrect Guid Format", 406));
             }
-
-
-
-
-            throw new NotImplementedException();
-            //return new FileStreamResult()
         }
 
         public IActionResult GetSavedFile(string submitted_document_id)
@@ -53,7 +54,20 @@ namespace Marine_Permit_Palace.Controllers
 
         public IActionResult GetNewNotPopulatedFile(string document_id)
         {
-            throw new NotImplementedException();
+            Guid id;
+            if (Guid.TryParse(document_id, out id))
+            {
+                //Grab the desired file
+                Document document = _DocumentSerivce.Get(id);
+                MemoryStream PDF_Mem = new MemoryStream(System.IO.File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "dist", "documents", document.TemplateName)));
+                return new FileStreamResult(PDF_Mem, "application/pdf");
+                //populate all the known fields
+                //return to the populated file
+            }
+            else
+            {
+                return Json(new Result("Failure", "Incorrect Guid Format", 406));
+            }
         }
 
         public JsonResult SaveFile(string custom_name, IFormFile pdf_doc)

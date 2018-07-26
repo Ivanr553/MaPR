91c7e7652d7df27bf5ac4fe99435a687b2b53663
Mitchell Marsh <mmarsh@scale7.co>
2018-07-25 21:40:51 -0700

    Updated method

--- Marine_Permit_Palace/Controllers/DocumentManager.cs
+++ Marine_Permit_Palace/Controllers/DocumentManager.cs
@@ -1,8 +1,10 @@
 ﻿using Marine_Permit_Palace.Models;
 using Marine_Permit_Palace.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -82,9 +84,43 @@ namespace Marine_Permit_Palace.Controllers
 
         //Assign Document -- Requires role - Supervisor
 
-        
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+        public class RequestSizeLimitAttribute : Attribute, IAuthorizationFilter, IOrderedFilter
+        {
+            private readonly FormOptions _formOptions;
+
+            public RequestSizeLimitAttribute(int valueCountLimit)
+            {
+                _formOptions = new FormOptions()
+                {
+                    // tip: you can use different arguments to set each properties instead of single argument
+                    KeyLengthLimit = (int)valueCountLimit,
+                    ValueCountLimit = (int)valueCountLimit,
+                    ValueLengthLimit = (int)valueCountLimit
+
+                    // uncomment this line below if you want to set multipart body limit too
+                    // MultipartBodyLengthLimit = valueCountLimit
+                };
+            }
+
+            public int Order { get; set; }
+
+            // taken from /a/38396065
+            public void OnAuthorization(AuthorizationFilterContext context)
+            {
+                var contextFeatures = context.HttpContext.Features;
+                var formFeature = contextFeatures.Get<IFormFeature>();
+
+                if (formFeature == null || formFeature.Form == null)
+                {
+                    // Setting length limit when the form request is not yet being read
+                    contextFeatures.Set<IFormFeature>(new FormFeature(context.HttpContext.Request, _formOptions));
+                }
+            }
+        }
 
         [HttpPost]
+        [RequestSizeLimit(valueCountLimit: 214748364)]
         public async Task<JsonResult> AssignDocument([FromBody]AssignDocumentData data)
         {
             if(data != null)

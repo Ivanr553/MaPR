6105ef059991acb5a85b2e3ffe861eb073cd3a95
ivanr553 <ivanr553@gmail.com>
2018-07-26 21:03:16 -0700

    Working with toolbar

--- Marine_Permit_Palace/Source/components/DocumentView/ToolBar/ToolBar.tsx
+++ Marine_Permit_Palace/Source/components/DocumentView/ToolBar/ToolBar.tsx
@@ -3,10 +3,26 @@ import * as React from 'react';
 const s = require('./ToolBarStyle.sass')
 
 class ToolBar extends React.Component {
+
+    tool() {
+
+        return (
+            <div className='toolbar-tool'>
+
+            </div>
+        )
+
+    }
+
     render() {
         return (
             <div className='ToolBar'>
                 <img className='tolkit-image' src="/images/toolkit.png" alt=""/>
+                <div className='tools-container'>
+                <div className='toolbar-tool' >
+                    <img className='toolbar-tool-image' src='/images/check.png' alt=""/>
+                </div>
+                </div>
             </div>
         );
     }
